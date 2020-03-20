42019580238f1a38967b6013f4ecc7ce21c2a488
Chris <mr.skytte@me.com>
2020-03-20 15:43:46 +0100

    Flubber tested and working (using D3 dependency as well), work with very complicated svg - will test further

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import "@babel/polyfill";
-
+let flubber = require("flubber");
+let d3 = require("d3");
 ("use strict");
 
 window.addEventListener("DOMContentLoaded", getSVG);
@@ -10,4 +11,21 @@ async function getSVG() {
   const response = await fetch("./svg/girl.svg");
   const girlSVG = await response.text();
   document.querySelector("#svg-container").innerHTML = girlSVG;
+  const girlOutline = document.querySelector("#Outline path").getAttribute("d");
+  const test = "M100,100 L200,100 L150,200Z";
+  const halfBox = "M10 10 L 10 300 300 300 Q 250 150 300 10";
+  console.log(girlOutline);
+  drawGirl(halfBox, girlOutline);
+}
+
+function drawGirl(originalLine, newLine) {
+  let myPathElement = document.querySelector("#changeThis");
+  let interpolator = flubber.interpolate(originalLine, newLine);
+
+  d3.select("path")
+    .transition()
+    .duration(1000)
+    .attrTween("d", function() {
+      return interpolator;
+    });
 }

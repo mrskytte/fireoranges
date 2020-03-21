c2e51414aee79d2fab18c4f1941eb5fac714b384
Chris <mr.skytte@me.com>
2020-03-21 23:53:48 +0100

    simple erase/draw function added - no morph

--- script.js
+++ script.js
@@ -6,25 +6,37 @@ window.addEventListener("DOMContentLoaded", getSVG);
 
 async function getSVG() {
   const response = await fetch("./svg/car.svg");
-  const bulbSVG = await response.text();
-  document.querySelector("#svg-container").innerHTML = bulbSVG;
+  const carSVG = await response.text();
+  document.querySelector("#main-svg").innerHTML = carSVG;
 
-  const parentSVG = document.querySelector("svg");
-  const originalPath = document.querySelector("#changeThis");
-  const innerSVG = document.querySelector("#pasteHere");
-  const carInline = document.querySelectorAll(".path");
-  const carOutline = document.querySelector("#outline").getAttribute("d");
-  const carOutlineClass = document.querySelector("#outline").classList[0];
-  const halfBox = originalPath.getAttribute("d");
+  window.addEventListener("click", drawSVG);
+  let counter = 0;
+  function drawSVG() {
+    if (counter === 0) {
+      document.querySelector(".car").classList.remove("active");
+      counter++;
+    } else {
+      document.querySelector(".car").classList.add("active");
+      counter = 0;
+    }
+  }
 
-  drawSVG(
-    originalPath,
-    innerSVG,
-    halfBox,
-    carOutline,
-    carOutlineClass,
-    carInline,
-    parentSVG,
-    "car"
-  );
+  // const parentSVG = document.querySelector("svg");
+  // const originalPath = document.querySelector("#changeThis");
+  // const innerSVG = document.querySelector("#pasteHere");
+  // const carInline = document.querySelectorAll(".path");
+  // const carOutline = document.querySelector("#outline").getAttribute("d");
+  // const carOutlineClass = document.querySelector("#outline").classList[0];
+  // const halfBox = originalPath.getAttribute("d");
+
+  // drawSVG(
+  //   originalPath,
+  //   innerSVG,
+  //   halfBox,
+  //   carOutline,
+  //   carOutlineClass,
+  //   carInline,
+  //   parentSVG,
+  //   "car"
+  // );
 }

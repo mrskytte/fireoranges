dca397d1cd4d405b3386d5e8f981892ca79d5a7f
kerttutimonen <kert0028@stud.kea.dk>
2020-03-26 08:25:12 +0100

    lightbulb

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 import "@babel/polyfill";
-import {
-  tranformSVG
-} from "./tranformSVG";
+import { tranformSVG } from "./tranformSVG";
 ("use strict");
 
 window.addEventListener("DOMContentLoaded", getSVGs);
@@ -23,7 +21,13 @@ function getSVGs() {
     const cityscapeSVG = getSVG("./svg/cityscape.svg");
     const carSVG = getSVG("./svg/car.svg");
 
-    const array = await Promise.all([timelineSVG, kiteSVG, bulbSVG, cityscapeSVG, carSVG]);
+    const array = await Promise.all([
+      timelineSVG,
+      kiteSVG,
+      bulbSVG,
+      cityscapeSVG,
+      carSVG
+    ]);
     return array;
   };
   SVGArray().then(svgs => {
@@ -54,7 +58,8 @@ function init() {
 
 function appendSVGs() {
   for (let i = 1; i < 6; i++) {
-    document.querySelector(`.container:nth-child(${i + 1})`).innerHTML = SVGs[i - 1];
+    document.querySelector(`.container:nth-child(${i + 1})`).innerHTML =
+      SVGs[i - 1];
   }
   timelineSVG = SVGs[0];
   kiteSVG = SVGs[1];
@@ -64,14 +69,18 @@ function appendSVGs() {
 }
 
 function drawInitialTimeline() {
-  document.querySelector("#main-svg").innerHTML = document.querySelector("#timeline-container").innerHTML;
+  document.querySelector("#main-svg").innerHTML = document.querySelector(
+    "#timeline-container"
+  ).innerHTML;
   parentSVG = document.querySelector("#main-svg svg");
   parentSVG.setAttribute("id", "primarySVG");
   setTimeout(() => {
     parentSVG.classList.add("active");
-    document.querySelector("#main-svg svg #outline").addEventListener("transitionend", () => {
-      document.querySelector("#first-circle").classList.add("active");
-    });
+    document
+      .querySelector("#main-svg svg #outline")
+      .addEventListener("transitionend", () => {
+        document.querySelector("#first-circle").classList.add("active");
+      });
   }, 100);
   document.querySelector("#timeline-btn").addEventListener("click", () => {
     animateEvent("timeline");
@@ -153,22 +162,31 @@ function animateKite() {
 }
 
 function animateBulb() {
-  //SET VALUE OF ANIMATION ELEMENT TO THE ANIMATION PATH / OBJECT YOU NEED TO USE
-  animationElement = "";
+  //SET VALUE OF ANIMATIONELEMENT TO THE ANIMATION PATH/OBJECT YOU NEED TO USE
+  animationElement =
+    '<path class="yellow" class="st0" d="M572.1,240.1c-38.1,36.3-58,87.4-76.4,135.6"/>';
+  ('<path class="blue" class="st1" d="M495.1,536.8c2.8,1.5,5.3,3.8,7.4,6.4c2.1,2.7,3.6,5.7,4.8,8.8c2.2,6.3,2.9,12.9,3.1,19.2s-0.3,12.5-0.4,18.5c-0.1,6-0.3,12,0.6,18.4c-4.1-5-6-11.6-7.1-17.9c-1.1-6.3-1.3-12.8-1.4-18.8c-0.1-6.2-0.3-12-1.1-17.8C500,548,498.3,542.4,495.1,536.8z"/>');
+  ('<path class="blue" class="st1" d="M824.3,211.7c2.9,2.8,5.2,5.6,7.3,8.5c2.1,2.9,4.1,5.9,5.6,9.1c1.7,3.2,3.1,6.6,4.1,10.2c1,3.6,1.7,7.4,1.1,11.3c-2.8-2.8-5-5.6-7.1-8.5s-3.9-5.9-5.6-9.1c-1.7-3.2-3.1-6.6-4.2-10.1C824.6,219.4,823.9,215.8,824.3,211.7z"/>');
+  ('<path class="blue" class="st1" d="M752.9,685.3c-1.4,3.1-3.2,5.6-5.3,7.7c-2.1,2.2-4.5,4.1-7.1,5.6c-2.7,1.5-5.6,2.7-8.8,3.1c-3.1,0.4-6.4,0.3-9.5-1c2.7-2.1,5-3.6,7.3-5.2c2.4-1.5,4.6-2.9,7-4.2c2.4-1.3,4.9-2.5,7.6-3.6S749.5,685.6,752.9,685.3z"/>');
+  ('<path id="light_on_support_wires" class="wires" d="M720.6,608.2c8.3-77.3,5.9-157.1,16.1-233.3c18.2-1.3,37-7.8,47.6-25.2c11.2-66.3-56.3-23.7-46.2,23.8c-11.6,5.9-30.5-2.4-44.1-4.2L676.1,384c-0.1-4.2,3.8-4.9,6-7c32.4-17.2-12.9-67.8-43.4-51.7c-16.5,3.8-53.5,43.4-28.7,52.1c-21.9-47.4-60.4-13.2-46.1,27.6c26.2,73.1,67.4,143.5,65,224.2"/>');
+  ('<path id="light_reflection_cable" class="wires" d="M672.9,870.2c-0.4,27.5,16.5,48.1,41.3,59.7c21.9,10.2,46.2,12.8,69.9,17.1c19.8,3.5,43.3,8.1,59.3,21.2c15.8,12.9,24.9,32.1,35.9,48.9c23.7,36.4,59.1,65.3,99.1,82.1c38.7,16.3,82,21.3,123.4,14.3"/>');
+  ('<path id="light_reflection_bulb_bottom" class="st0" d="M684.9,723.8c81.5,31.9,11.2,63.9-53.8,63.9c-3.8,0-6.6,3.4-5.6,7c6.7,25.8,41.5,18.6,66.6,46.5"/>');
+
   return animationElement;
 }
 
 function animateCityscape() {
   //SET VALUE OF ANIMATION ELEMENT TO THE ANIMATION PATH/OBJECT YOU NEED TO USE
-  animationElement = '<path id="yellow-line" class="st0" d="M69.9,873.1c33.4-1,77.9,2.8,98.8,31.7c0.3,0.5,0.8,0.9,1.3,1.4l0,0c1,0.8,1.9,2,2.3,3.3c9.3,29.6-13.1,63.2-40.4,75.8c-4.5,2.1-9.5,3-14.4,2.2c-28.5-4.4,3.8-49.4,18.7-60.8c25.3-22.3,60-30,92.4-35.3c46-5.9,92.7-5.6,139-2.7c28.8,1.9,53.9-19.1,57.5-47.7c5.5-44.3,1.3-101,66.1-115.4c37.7,2.7,42.8-72.1,70-64.3c3.8,1,6.9,4,8.5,7.4c8.3,17.4,22,27.4,42.3,28.1c61.5,12.6,38.2,8.1,70.9-25.1c2.4-2.6,5.8-4.2,9.3-4.8c46.8-7,53.3-37.3,75.7-71c31.8-19.1,43.2,12.1,68.6,19.1c70.9,15.7,94.6-29.1,94.7-92.9c-1.4-47.4,51.8-61.6,67.4-100c17.1-60.3,17.5-124.2,22.9-185.5l0,0l4.3,42.4l0,0c14.9,20.6,7.8,51.5,23.9,72.3c1.2,1.5,2.4,2.8,3.7,4.2c18.6,18.9,21.7,42.5,22.2,67.2c0.2,13.5,4.1,26.7,11.4,38.1c34.9,54.9,44.9,32.1,48.7,141.3c3.6,19.4-2.2,44.7,11.6,60.3c36,46.7,24.8,117.4,40.1,173.3c6.5,24.5,57.2,75.4,58,23.2c14.8-46.6,52.6-80.4,47.8-134.8c91.9-229.5,161,35.7,188.5-151.6c1.3-9.1,12-13.2,19.1-7.7l0,0c2.7,2.1,4.4,5.2,4.5,8.7c5.8,109.6,189.9,28.6,208.6,142.8c48.7,132.3-14.2,161.6,163.8,185.4l125.5,35c-36.1-120.5,161.4-109,7-0.6l1.7,3.5c77.6,20.7,155.4-55.1,225.5-83.7"/>';
-  '<path id="blue_right_bottom" class="blueline" d="M291.9,848.1c-18.5-0.9-37.7-0.7-55.8-4.1"/>';
-  '<path id="blue_left_bottom" class="blueline" d="M2217.7,949.8c-16.5,8.7-33.1,20.3-52.3,22.1"/>';
-  '<path id="blue_bottom" class="blueline" d="M1715.6,975.3c-76.2,0.6-152.6,0.3-229,0"/>';
-  '<path id="blue_top_left" class="blueline" d="M733.5,478.3c10.3,9.4,18.6,21.7,29.1,30.6"/>';
-  '<path id="blue_left_down_bottom" class="blueline" d="M80,907.4c3.3,12.6-2.1,22.8-8.4,33.1"/>';
-  '<path id="blue_right_top" class="blueline" d="M1838.2,749.9c2.4,13.5,1.4,27.3,4.1,40.7"/>';
-  '<path id="blue_top_top" class="blueline" d="M1039.8,259.4c0.6,10.5,1.2,20.9,1.7,31.4"/>';
-  '<path id="blue_top_right" class="blueline" d="M1533.1,534.3c3.4-0.3,6.2,1.6,9.3,2.3"/>';
+  animationElement =
+    '<path id="yellow-line" class="st0" d="M69.9,873.1c33.4-1,77.9,2.8,98.8,31.7c0.3,0.5,0.8,0.9,1.3,1.4l0,0c1,0.8,1.9,2,2.3,3.3c9.3,29.6-13.1,63.2-40.4,75.8c-4.5,2.1-9.5,3-14.4,2.2c-28.5-4.4,3.8-49.4,18.7-60.8c25.3-22.3,60-30,92.4-35.3c46-5.9,92.7-5.6,139-2.7c28.8,1.9,53.9-19.1,57.5-47.7c5.5-44.3,1.3-101,66.1-115.4c37.7,2.7,42.8-72.1,70-64.3c3.8,1,6.9,4,8.5,7.4c8.3,17.4,22,27.4,42.3,28.1c61.5,12.6,38.2,8.1,70.9-25.1c2.4-2.6,5.8-4.2,9.3-4.8c46.8-7,53.3-37.3,75.7-71c31.8-19.1,43.2,12.1,68.6,19.1c70.9,15.7,94.6-29.1,94.7-92.9c-1.4-47.4,51.8-61.6,67.4-100c17.1-60.3,17.5-124.2,22.9-185.5l0,0l4.3,42.4l0,0c14.9,20.6,7.8,51.5,23.9,72.3c1.2,1.5,2.4,2.8,3.7,4.2c18.6,18.9,21.7,42.5,22.2,67.2c0.2,13.5,4.1,26.7,11.4,38.1c34.9,54.9,44.9,32.1,48.7,141.3c3.6,19.4-2.2,44.7,11.6,60.3c36,46.7,24.8,117.4,40.1,173.3c6.5,24.5,57.2,75.4,58,23.2c14.8-46.6,52.6-80.4,47.8-134.8c91.9-229.5,161,35.7,188.5-151.6c1.3-9.1,12-13.2,19.1-7.7l0,0c2.7,2.1,4.4,5.2,4.5,8.7c5.8,109.6,189.9,28.6,208.6,142.8c48.7,132.3-14.2,161.6,163.8,185.4l125.5,35c-36.1-120.5,161.4-109,7-0.6l1.7,3.5c77.6,20.7,155.4-55.1,225.5-83.7"/>';
+  ('<path id="blue_right_bottom" class="blueline" d="M291.9,848.1c-18.5-0.9-37.7-0.7-55.8-4.1"/>');
+  ('<path id="blue_left_bottom" class="blueline" d="M2217.7,949.8c-16.5,8.7-33.1,20.3-52.3,22.1"/>');
+  ('<path id="blue_bottom" class="blueline" d="M1715.6,975.3c-76.2,0.6-152.6,0.3-229,0"/>');
+  ('<path id="blue_top_left" class="blueline" d="M733.5,478.3c10.3,9.4,18.6,21.7,29.1,30.6"/>');
+  ('<path id="blue_left_down_bottom" class="blueline" d="M80,907.4c3.3,12.6-2.1,22.8-8.4,33.1"/>');
+  ('<path id="blue_right_top" class="blueline" d="M1838.2,749.9c2.4,13.5,1.4,27.3,4.1,40.7"/>');
+  ('<path id="blue_top_top" class="blueline" d="M1039.8,259.4c0.6,10.5,1.2,20.9,1.7,31.4"/>');
+  ('<path id="blue_top_right" class="blueline" d="M1533.1,534.3c3.4-0.3,6.2,1.6,9.3,2.3"/>');
   return animationElement;
 }
 
